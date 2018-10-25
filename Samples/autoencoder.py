639db505dbcfd6840895b02bf1f8667592b74cdf
Melis ÖZYILDIRIM <bmozyildirim@gmail.com>
2018-10-25 19:07:37 +0300

    matlab/tensorflow

---
+++ Samples/autoencoder.py
@@ -0,0 +1,63 @@
+
+from __future__ import print_function, division
+from builtins import range,input
+from sklearn.utils import shuffle
+
+
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+class AutoEncoder(object):
+    def __init__(self,D,M):
+        self.M = M
+        self.build(D,M)
+
+    def build(self, D, M):
+        self.W = tf.Variable(tf.random_normal(shape=(D,M)))
+        self.bh = tf.Variable(tf.zeros(M))
+        self.bo = tf.Variable(tf.zeros(D))
+
+        self.X_in = tf.placeholder(tf.float32, shape=(None,D))
+        self.Z = tf.nn.sigmoid(tf.matmul(self.X_in,self.W)+self.bh)
+        self.X_hat = tf.nn.sigmoid(tf.matmul(self.Z,tf.transpose(self.W))+self.bo)
+        self.cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=self.X_in, logits=self.X_hat))
+        self.train_op = tf.train.AdamOptimizer(1e-1).minimize(self.cost)
+
+    def fit(self,X,sess,epochs=1, batch_sz = 100):
+        self.session = sess
+        N,D = X.shape
+        n_batches = N//batch_sz
+        costs=[]
+        for i in range(epochs):
+            X=shuffle(X)
+            for j in range(n_batches):
+                batch = X[j*batch_sz:(j*batch_sz + batch_sz)]
+                _,c =self.session.run((self.train_op, self.cost), feed_dict={self.X_in:batch})
+                print(c)
+                costs.append(c)
+        plt.plot(costs)
+        plt.show()
+#Test with MNIST dataset
+def getKaggleMNIST():
+
+    train = pd.read_csv('train.csv').as_matrix().astype(np.float32)
+    train = shuffle(train)
+
+    Xtrain = train[:-1000,1:] / 255
+    Ytrain = train[:-1000,0].astype(np.int32)
+
+    Xtest  = train[-1000:,1:] / 255
+    Ytest  = train[-1000:,0].astype(np.int32)
+    return Xtrain, Ytrain, Xtest, Ytest
+
+Xtrain, Ytrain, Xtest, Ytest = getKaggleMNIST()
+Xtrain = Xtrain.astype(np.float32)
+Xtest = Xtest.astype(np.float32)
+_,D = Xtrain.shape
+encod = AutoEncoder(D,300)
+init = tf.global_variables_initializer()
+with tf.Session() as sess:
+    sess.run(init)
+    encod.fit(Xtrain,sess,50)
---
+++ Samples/thirdgcnn.m
@@ -0,0 +1,65 @@
+function [overall,accuracy,error,classifiedrate] = thirdgcnn(xtrain,ytrain,xtest,V,nclass,classes)
+%xrain and xtest are data, ytrain onehotencoded (0.1,0.9) labels
+%classes class information to calculate fmeasure
+%V calculated smoothing parameter
+%nclasses #ofclasses
+
+
+[m,n] = size(xtrain);  %m #of features, n #of element the last column is class
+[mt,nt] = size(xtest); %mt %of features, n #of element the last column is class  
+
+%fprintf('############################GCNN######################\n');
+%for each test sample, calculate similarity and choose the most similar
+%sample in training set
+    for p = 1:nt  
+        dist = sum(((xtest(1:m-1,p)*ones(1,n))- xtrain(1:m-1,:)).^2);   
+        pl = exp(-1*(dist./(2.*(V.^2))));
+        nom = pl*(exp(ytrain - (0.9*ones(nclass,n))).*ytrain)';
+        denom = sum(pl);
+        out = nom ./ denom;               
+       [result(p),ind(p)] = max(out); 
+       clear nom pl dist denom out
+    end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Create confusion matrix for fmeasure %%
+    confmat = zeros(nclass,nclass);
+    trues = zeros(nclass,1);
+    falses = zeros(nclass,1);
+    elemans = zeros(nclass,1);
+
+    for l = 1 : nt
+        if(xtest(mt,l) == classes(ind(l)))
+            confmat(ind(l),ind(l)) =  confmat(ind(l),ind(l)) + 1;
+            trues(ind(l)) = trues(ind(l)) + 1;
+            elemans(ind(l)) = elemans(ind(l)) + 1;
+        else
+            indt = find(classes == xtest(mt,l));
+          confmat(indt,ind(l)) =  confmat(indt,ind(l)) + 1;
+          falses(indt) = falses(indt) + 1;
+          elemans(indt) = elemans(indt) + 1;
+          clear indt
+        end
+    end
+    %%%% Call fmaeasure function: if there are more than two classes,
+    %%%% overall performance is calculated as average of fmeasures
+       overall = fmeasure(nclass,elemans,trues,falses);
+        error = 0;
+        accuracy = 0;
+        classifiedrate = 0;
+     % to calculate classification performance predefined function
+     % classperf is used
+      if(any(xtest(mt,:) ~= xtest(mt,1)))
+           cp = classperf(xtest(mt,:),classes(ind));
+           error =cp.ErrorRate
+           accuracy =cp.CorrectRate
+           classifiedrate=cp.ClassifiedRate    
+           % if some results approache to Nan value, accuracy will be 0
+           if (isnan(accuracy))
+               error = 1;
+               accuracy = 0;
+               classifiedrate = 0;
+           end
+      end
+end
+
+
+
